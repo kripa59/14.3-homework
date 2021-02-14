312b80dcefa6ebb9e31abfa448209b273db3f9eb
kripa59 <kripa59@yandex.ru>
2021-02-14 22:30:44 +0300

    14.3 homework

---
+++ homework 14.3/homework 14.3/homework 14.3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Enter your company: ";
+	std::string company;
+	std::getline(std::cin, company);
+	std::cout << company.size() << std::endl;
+	std::cout << "first symbol: "<< company[0] << "\n" ;
+	std::cout << "last symbol: "<< company.substr(company.size() - 1, 1);
+
+	std::cin;
+	return 0;
+}
